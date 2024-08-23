a058f5055f57305961bd71ffdedc24c6b36db961
Liza Baez Kemp <baezkemp@gmail.com>
2024-08-22 22:22:29 -0500

    First Commit

---
+++ Manager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePayrollSoftware
+{
+    internal class Manager : Staff
+    {
+        // fields
+        private const float managerHourlyRate = 50;
+
+        // properties
+        public int Allowance { get; private set; }
+
+        // constructor
+        public Manager(string name) : base(name, managerHourlyRate) { }
+
+        // methods
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            Allowance = 1000;
+
+            if (HoursWorked > 160) TotalPay += Allowance;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SimplePayrollSoftware
+{
+    internal class Staff
+    {
+        // fields
+        private float hourlyRate;
+        private int hWorked;
+
+        // properties
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+        public int HoursWorked
+        {
+            get { return hWorked; }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        // contstructor
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+
+        // methods
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "\nName of Staff: " + NameOfStaff + "\nHourly Rate: " + hourlyRate + "\nHours Worked: " + hWorked + "\nBasic Pay: " + BasicPay + "\n\nTotal Pay: " + TotalPay;
+        }
+    }
+}
